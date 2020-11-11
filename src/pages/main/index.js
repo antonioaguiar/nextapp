bf531cb04d1d8717b3db039452401b8249a56295
Antonio Aguiar <antonioaguiar@gmail.com>
2020-11-11 20:51:29 +0000

    paginacao

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,28 +4,39 @@ import api from '../../services/api';
 export default class Main extends Component {
 
     state = {
+        pagina: 1,
         products: [],
     };
 
     //executa assim que monta a tela
     componentDidMount() {
-        this.loadProducts();
+        this.loadProducts(1);
     }
 
     loadProducts = async () => {
-        const response = await api.get('/products');
-        console.log(response.data.docs);
+        const response = await api.get(`/products?page=${this.state.pagina}`);
+        //console.log(response.data.docs);
         this.setState({ products: response.data.docs });
     }
 
+    navegarPaginasClick(newPage) {
+        this.setState({ pagina: newPage });
+    };
+ 
+
     render() {
         return (
-            //<h1>Produtos: {this.state.products.length}</h1>
-            <div className="product-list">
-                {this.state.products.map(product => (
-                    <h2 key={product._id}>{product.title}</h2>
-                ))}
+            <div>
+                <h1>Produtos: {this.state.products.length}</h1>
+                <div className="product-list">
+                    {this.state.products.map(product => (
+                        <h2 key={product._id}>{product.title}</h2>
+                    ))}
+                </div>
+                <button onClick={this.navegarPaginasClick(2)}>
+                    Página Atual: {this.state.pagina}
+                </button>
             </div>
-        )
+        );
     }
 }
