c8804cc651d71c4dab76ea823921d9cdf60fbc58
Antonio Aguiar <antonio.aguiar@abaco.com.br>
2020-11-11 11:50:36 -0400

    next 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import App from './App';
+
+function Home() {
+    return <App / >
+}
+
+export default Home;
+
+
+//ReactDOM.render( < App / > , document.getElementById('root'), );
